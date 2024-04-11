28b40fedfd510c76fc7427f99e0cc5a7e11086b7
berker-kync <berkerkoyuncu@gmail.com>
2024-04-11 22:48:05 +0300

    iki videoyu merge eden V1 proje ffmpeg ile  çalıştırıldı

--- TestProjects/video-converter.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-command = ["ffmpeg", "-i", "/Users/berkerkoyuncu/Desktop/Project/Videos/Video1.mp4", "-c:v", "libx264", "-c:a", "copy", "/Users/berkerkoyuncu/Desktop/Project/Videos/output.avi"]
-
-subprocess.run(command)
